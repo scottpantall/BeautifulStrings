921b7851d948257888fa64a53d9039a6f5f131aa
Scott Pantall <aspantall@gmail.com>
2017-05-30 18:55:29 -0600

    Successfully creates a map of values for each letter in string.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@
  */
 import java.io.*;
 import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main (String[] args) throws IOException {
@@ -18,6 +23,41 @@ public class Main {
     }
     
     public static void BeautifulString(String line) {
-        System.out.println(line);
+        //System.out.println(line.length());
+        char[] chars = line.toLowerCase().toCharArray();
+        //boolean blah = Character.isLetter('.');
+        HashMap countMap = new HashMap(); //key = letter, value = count of that letter
+        
+        // Get first character
+        // If character is a letter...
+            // Count how many times that letter is in the string.
+            // Put that result into a Collection
+        // Get next character
+        // If character is a letter...
+            // Checkt to see if it's in the Collection
+            // If not then
+            // Count how many times that letter is in the string.
+            // Put that result into a Collection
+        // Get next character
+        // and on and on...
+        
+        for(int i = 0; i < chars.length; i++) {
+            
+            // If the character is a letter and is not already in the countMap
+            if(Character.isLetter(chars[i]) && !countMap.containsKey(chars[i])) {
+                int count = 0;
+                for(int j = 0; j < chars.length; j++) {
+                    if(chars[i] == chars[j]) {
+                        count++;
+                    }
+                }
+                
+                countMap.put(chars[i], count);
+            }
+        }
+        
+        System.out.println(countMap.size());
+        System.out.println(countMap.values());
+
     }
 }
